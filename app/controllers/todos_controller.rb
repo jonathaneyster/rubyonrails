7df91d45b6b895f343aef1a8b951fb579b49244f
EC2 Default User <ec2-user@ip-172-31-38-155.us-east-2.compute.internal>
2018-12-29 20:18:37 +0000

    initial commit

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,50 @@
+class TodosController < ApplicationController
+  def new
+    @todo = Todo.new
+  end
+  
+  def create
+    @todo = Todo.new(todo_params)
+    if
+      @todo.save
+      flash[:notice]= "Todo was created sucsessfully."
+      redirect_to todo_path(@todo)
+    else
+      render "new"
+    end
+  end
+  
+  private def todo_params
+    params.require(:todo).permit(:name, :description)
+  end
+  
+  def show
+    @todo = Todo.find(params[:id])
+  end
+  
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+  
+  def update
+     @todo = Todo.find(params[:id])
+     if
+      @todo.update(todo_params)
+      flash[:notice] = "Todo was updated"
+      redirect_to todo_path(@todo)
+    else
+      render 'edit'
+    end  
+  end
+  
+  def index
+    @todos = Todo.all
+  end
+  
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    flash[:notice] = "Todo was deleted"
+    redirect_to todos_path
+  end
+end
